cc7841a1110bfe1219d5147f6e28c3b1184bad9c
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-10-27 15:17:03 +0200

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover",(event)=>{
+    const x = (event.pageX - btnEl.offsetLeft);
+    const y = (event.pageY - btnEl.offsetTop);
+
+    btnEl.style.setProperty("--xPosition", x + "px");
+    btnEl.style.setProperty("--yPosition", y + "px");
+})
